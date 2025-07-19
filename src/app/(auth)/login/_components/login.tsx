e459a31993201e69a9b7a4fb4d08991ef13411a8
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-07-19 18:55:08 +0700

    refactor for form input

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import FormInput from "@/components/ui/common/form-input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
@@ -26,32 +27,8 @@ export default function Login() {
       <CardContent>
         <Form {...form}>
           <form onSubmit={onSubmit} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field: { ...rest } }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input {...rest} type="email" placeholder="Insert your email" autoComplete="off" />
-                  </FormControl>
-                  <FormMessage className="text-xs" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field: { ...rest } }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...rest} type="password" placeholder="******" autoComplete="off" />
-                  </FormControl>
-                  <FormMessage className="text-xs" />
-                </FormItem>
-              )}
-            />
+            <FormInput form={form} name="email" label="Email" placeholder="Insert email here" type="email" />
+            <FormInput form={form} name="password" label="Password" placeholder="******" type="password" />
             <Button type="submit">Login</Button>
           </form>
         </Form>
