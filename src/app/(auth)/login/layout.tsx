781d3b204bde3f81d28805a5ff70873a18b530de
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-08-06 08:30:03 +0700

    make the dashboard layout

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../../globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
