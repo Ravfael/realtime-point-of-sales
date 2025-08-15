8fa87707787f72372ef5e0fb404788208c4f7997
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-08-15 09:19:39 +0700

    create order list

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "Ravfael Cafe | Order Management",
+};
+
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
---
+++ src/constants/order-constant.ts
@@ -0,0 +1,1 @@
+export const HEADER_TABLE_ORDER = ["No", "Order ID", "Customer Name", "Table", "Status", "Action"];
