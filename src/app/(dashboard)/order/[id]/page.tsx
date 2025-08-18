8065591695e12b26b52a527e3965344d05dd8634
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2025-08-18 20:02:54 +0700

    create detail order page

---
+++ src/app/(dashboard)/order/[id]/page.tsx
@@ -0,0 +1,10 @@
+import DetailOrder from "./_components/detail-order";
+
+export const metadata = {
+  title: "Ravfael Cafe | Detail Order",
+};
+
+export default async function DetailOrderPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <DetailOrder id={id} />;
+}
